7136cd0708b235ccafa90a6dd258c924b44b5ac1
8bitpineapple <bradley.pymm@gmail.com>
2023-11-10 03:47:57 +0000

    Add files via upload

---
+++ Block_Steam.bat
@@ -0,0 +1,69 @@
+@echo off
+set "RuleName=Steam_Internet"
+
+:: Check if the firewall rule exists
+netsh advfirewall firewall show rule name="%RuleName%" > nul 2>&1
+
+if %errorlevel% equ 0 (
+    :: Firewall Rule Exists
+    echo Firewall rule "%RuleName%" exists.
+
+    :: Check if the rule is enabled
+    netsh advfirewall firewall show rule name="%RuleName%" | find "Enabled:                              No" > nul
+
+    if %errorlevel% equ 0 (
+	echo Enabling Internet Access for Steam
+        netsh advfirewall firewall set rule name="%RuleName%" new enable=no
+        echo Internet access available.
+	goto Done
+
+    ) else (
+	echo Disabling Internet Access for Steam
+        netsh advfirewall firewall set rule name="%RuleName%" new enable=yes
+        echo Internet access disabled.
+	goto Done
+
+    )
+) else (
+:: Firewall Rule Doesn't Exist
+    echo Firewall rule "%RuleName%" does not exist.
+    goto FindPath
+)
+
+
+:FindPath
+setlocal enabledelayedexpansion
+
+:: Find Steam installation folder from Windows registry
+set "SteamRegKey="
+if defined ProgramFiles(x86) set "SteamRegKey=HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Valve\Steam"
+if not defined ProgramFiles(x86) set "SteamRegKey=HKEY_LOCAL_MACHINE\SOFTWARE\Valve\Steam"
+
+set "SteamExePath="
+for /f "tokens=2*" %%a in ('reg query "%SteamRegKey%" /v InstallPath ^| find "InstallPath"') do set "SteamExePath=%%b"
+
+if not defined SteamExePath (
+    echo Steam not found in the registry.
+    goto :Done
+)
+
+:: Append \steam.exe to the installation path
+set "SteamExePath=!SteamExePath!\steam.exe"
+
+:: Display Steam installation path
+echo Steam executable is located at: %SteamExePath%
+
+:: Check if the rule already exists
+netsh advfirewall firewall show rule name="%RuleName%" > nul 2>&1
+
+if %errorlevel% neq 0 (
+    echo Creating firewall rule to block Steam executable.
+    netsh advfirewall firewall add rule name="%RuleName%" dir=out program="%SteamExePath%" action=block
+    echo Firewall rule created.
+) else (
+    echo Firewall rule "%RuleName%" already exists. No action taken.
+)
+
+:Done
+pause
+exit /b
